9904d388cf8ef5033b0079cb1069781fa5d91169
angelicty <muyenziraissa@gmail.com>
2019-08-04 13:57:07 +0200

    getting the html value

--- js/scripts.js
+++ js/scripts.js
@@ -22,5 +22,58 @@ function Order(pizzas,size,topp,crust,quantity,deliver) {
    // user interface logic
    $(document).ready(function() {
 
-    $("button#").click(function() {
-  
+    $("button#orderNow").click(function() {
+       //pizza name
+       if($('#pizza1:checked').val()){
+        var prix='10';
+        var size=$('#pizza2').val(); + " "+ "price=$2"
+        console.log();
+    }
+    if($('#pizza2:checked').val()){
+        var prix='$12';
+        var size=$('#pizza2').val(); + " "+ "price=$2"
+        console.log();
+    }
+    if($('#pizza3:checked').val()){
+        var prix='$15';
+        var size=$('#pizza3').val(); + " "+ "price=$2"
+        console.log();
+    }
+    if($('#pizza4:checked').val()){
+        var prix='$13';
+        var size=$('#pizza4').val(); + " "+ "price=$2"
+        console.log();
+    }
+    if($('#pizza3:checked').val()){
+        var prix='$15';
+        var size=$('#pizza5').val(); + " "+ "price=$2"
+        console.log();
+    }
+    if($('#pizza3:checked').val()){
+        var prix='$10';
+        var size=$('#pizza6').val(); + " "+ "price=$2"
+        console.log();
+    }
+       
+       
+       
+       
+       
+        //pizza size
+        if($('#small:checked').val()){
+            var prix='$2';
+            var size=$('#small').val(); + " "+ "price=$2"
+            console.log();
+        }
+        if($('#medium:checked').val()){
+            var prix='$2';
+            var size=$('#medium').val(); + " "+ "price=$2"
+            console.log();
+        }
+        if($('#large:checked').val()){
+            var prix='$2';
+            var size=$('#large').val(); + " "+ "price=$2"
+            console.log();
+        }
+
+
